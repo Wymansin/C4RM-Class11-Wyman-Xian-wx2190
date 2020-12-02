5c0e8f11f0173ed4e78e44b158aff3d07ff06ff1
Wymansin <wymansin@github.com>
2020-12-02 15:01:56 -0500

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -2,14 +2,14 @@
 def getBondDuration(y, face, couponRate, m, ppy = 1):
   cf = face * couponRate
   pvcf = 0
-  t = 1
+  t = 0
   tot = 0
   pvcft = 0
   
   for t in range(m):
-    pvcf = cf * (1 + y) ** -t
+    pvcf = cf * (1 + y) ** -(t+1)
     tot = tot + pvcf
-    pvcft = pvcft + pvcf * t
+    pvcft = pvcft + pvcf * (t+1)
   
   tot = tot + face * (1 + y) ** -m
   pvcft = pvcft + (face * (1 + y) ** -m) * m
