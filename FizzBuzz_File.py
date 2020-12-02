483c8e3a369ba5ec0a3db0a892b809cb28a67b93
Wymansin <wymansin@github.com>
2020-12-02 16:00:03 -0500

    v1

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,25 @@
 
 
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 44, 'fizzbuzz']
-    return(v)
+
+  n = finish-start+1
+  v = []
+  i = 0
+  
+  for i in range(n):
+    if ((i + start) % 3 == 0 & (i + start) % 5 == 0):
+      v.append("fizzbuzz")
+    
+    
+    elif ((i + start) % 5 == 0):
+      v.append("Buzz")
+    
+    elif ((i + start) % 3 == 0):
+      v.append("Fizz")
+    
+    else:
+      v.append(i + start)
+
+  print(v)
+
+  return(v)
