d21579b4ca2d522c6e91bc33127b0cf65a4abb06
Wymansin <wymansin@github.com>
2020-12-02 15:27:32 -0500

    v2

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,14 @@
 
 
 def getBondPrice_E(face, couponRate, m, yc):
-    return(2098949)
+
+ coup = face * couponRate
+ 
+ for index,value in enumerate(yc):
+  pv = (1 + value) ** - (index + 1) 
+  
+ Bond = coup * (sum(pv) + pv[-1])
+
+ return (Bond)
+
+
