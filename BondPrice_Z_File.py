4d8eaec7364ba20c5fff0ff957ff451790830ea5
Wymansin <wymansin@github.com>
2020-12-02 15:47:56 -0500

    v2

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -9,11 +9,3 @@ def getBondPrice_Z(face, couponRate, times, yc):
     Bond = coup * sum(pv) + pv[-1] * face
     
     return(Bond)
-
-yc = [.010,.015,.020,.025,.030]
-times=[1,1.5,3,4,7]
-face = 2000000
-couponRate = .04
-
-
-z = getBondPrice_Z(face, couponRate, times, yc)
