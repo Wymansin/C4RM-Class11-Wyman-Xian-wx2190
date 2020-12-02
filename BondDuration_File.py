f0e0f4206bf852bbb1da6807f5adc44a1dc65a2f
Wymansin <wymansin@github.com>
2020-12-02 14:35:39 -0500

    v2

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,18 @@
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+  cf = face * couponRate
+  pvcf = 0
+  t = 1
+  tot = 0
+  pvcft = 0
+  
+  for t in range(m):
+    pvcf = cf * (1 + y) ^ -t
+    tot = tot + pvcf
+    pvcft = pvcft + pvcf * t
+  
+  tot = tot + face * (1 + y) ** -m
+  pvcft = pvcft + (face * (1 + y) ** -m) * m
+  Bond = tot
+  Duration = pvcft / Bond
+  return (Duration)
