227727f702f386d0188480f2efeda948b91055c5
Wymansin <wymansin@github.com>
2020-12-02 13:59:51 -0500

    v3

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -4,5 +4,5 @@ def getBondPrice(y, face, couponRate, m, ppy=1):
  coup = face * couponRate / ppy
  m = m * ppy
  y = y / ppy
- Bond = (coup * (1 - (1 + y) ^ (-m)) / (y)) + face * (1 + y) ^ (-m)
+ Bond = (coup * (1 - (1 + y) ** (-m)) / (y)) + face * (1 + y) ** (-m)
  return (Bond)
